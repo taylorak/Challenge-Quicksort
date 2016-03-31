650a96547d9be4b6c0d27cf263c00726224e176a
Taylor Kennedy <taykenned@gmail.com>
2016-03-30 15:09:44 -1000

    adds tests for quicksort

--- test/quicksort.spec.js
+++ test/quicksort.spec.js
@@ -3,7 +3,19 @@ const expect = chai.expect;
 const quicksort = require('../quicksort.js');
 
 describe("Quicksort", function() {
+  var unsortedArray;
+  var sortedArray = [1, 2 , 3, 4, 5];
+
+  beforeEach(function() {
+    unsortedArray = [5, 4, 3 , 2, 1]
+  })
+
   it('should be a function', function() {
     expect(quicksort).to.be.a('function');
+  })
+
+  it('should take an input Array and apply quicksort', function() {
+    quicksort(unsortedArray, 0, unsortedArray.length - 1);
+    expect(unsortedArray).to.deep.equal(sortedArray);
   })
 })
